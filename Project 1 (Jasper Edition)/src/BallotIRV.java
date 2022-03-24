92bfcc14de63d76aa39eac474173702286cb31b6
beaul116 <beaul116@umn.edu>
2022-03-24 01:14:08 -0500

    ElectionIRV, BallotIRV
    Some introduction stuff. Have some design choices going to need to design soon aka how far from the diagrams and documents can we get

--- Project 1 (Jasper Edition)/src/BallotIRV.java
+++ Project 1 (Jasper Edition)/src/BallotIRV.java
@@ -1,3 +1,24 @@
-public class BallotIRV
-{
+
+
+public class BallotIRV {
+
+    /** 
+     * Creates a BallotIRV object
+     * @param rank_arr populates the ballot with the orders in which the candidates are ranked
+     */
+    public BallotIRV(int rank_arr) {
+
+        private Queue<integer> ranking; // Need to autopopulate the ranking queue with the parameter.
+                                        // Becomes trickier with ballots that don't have all four candidates ranked.
+                                        // Can either do most computation in here, or do most in ElectionIRV and 
+                                        // do a simple queue add based on the array passed in. Shana mentioned in class
+                                        // how for IRV, it might be helpful to assign numbers to the candidates. 
+
+    }
+
+    public nextCandidate() {
+        // todo
+        // Not sure what this means, but it's in the class diagram
+    }
+
 }
--- Project 1 (Jasper Edition)/src/ElectionIRV.java
+++ Project 1 (Jasper Edition)/src/ElectionIRV.java
@@ -18,15 +18,88 @@ public class ElectionIRV extends Election
      */
     public ElectionIRV()
     {
-
+        super(fileHandler);
     }
 
     /**
      * Calculates the results of the election according to IRV
      */
     @Override
-    public void calcResults()
-    {
+    public void calcResults() {
+
+        if (inputFileExists()) {
+
+            String electionType = nextLine();
+
+            if (electionType.equals("IRV")) {
+
+                numCandidates = nextInt();
+                numBallots = nextInt();
+                String rawCandidates = nextLine();
+
+                if (!getCandidatesFromIRVLine(rawCandidates)) {
+                    // error
+                }
+
+            }
+
+            // Need to handle shuffle check here
+            // Possibly add shuffle as a function in ElectionIRV
+
+            // While there are still unassigned ballots
+            while (numBallots != 0) {
+
+            }
+
+
+            
+        }
+    }
+
+    public boolean getCandidatesFromIRVLine(String line) {
+        
+        // split the raw input line into an array of candidates names
+        String[] candidates_arr = line.split(",");
+
+        // populate the candidates arrayList with candidates
+        for (int i = 0; i < candidates_arr.length; i++) {
+            candidates.add(new CandidateIRV(candidates_arr[i]));
+        }
+
+        return true;
+    }
+
+    /**
+     * Reads through all the ballots and assigns the ballots to their first choice candidate
+     */
+
+    // Note - might not need to loop, and can instead read line by line. However, it can also be
+    // helpful to do it all in one pass, check for a majority, and then go reassigning ballots
+    public boolean readFirstChoiceBallots() {
+
+        // read each line
+        for (int i = 0; i < numBallots; i++) {
+
+            // Read in a ballot, and populate an array with the choices. (Now, does split recognize choices like ",,1,2" and populate
+            // the array with 4 different objects? Not sure)
+            String rawBallot = nextLine();
+            int[] arr choice_arr = rawBallot.split(','); 
+
+            // Create a new ballot object. Notes on population in BallotIRV
+            BallotIRV ballot = new BallotIRV(choice_arr);
+
+            // Find the index of the choice_arr that contains 1, find the candidate that is in the candidate list array 1, and add the
+            // ballot to that candidate
+            int index = choice_arr.indexOf(1);
+            CandidateIRV candidate = (CandidateIRV) candidates.get(index);
+            candidate.ballots.add(ballot);
+
+        }
+
+        // return 1 if success
+        return 1;
 
     }
+
+
 }
