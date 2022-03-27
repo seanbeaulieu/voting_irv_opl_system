498537c05074946fb44cd2df59cd4a4ea214a0a1
sheypemberton <sheypemberton@gmail.com>
2022-03-27 17:51:41 -0500

    Update CandidateTest.java

--- Project1 junit/src/test/java/Candidates/CandidateTest.java
+++ Project1 junit/src/test/java/Candidates/CandidateTest.java
@@ -18,52 +18,20 @@ class CandidateTest
     @Test
     void getName()
     {
-<<<<<<< HEAD
-       /* Name testClass = new Name();
-
-        testClass.setmName("Jasper");
-        assertEquals("Jasper", testClass.getmName());
-
-        testClass.setfName("Shey");
-        assertEquals("Shey", testClass.getfName());*/
-=======
-//        Name testClass = new Name();
-//
-//        testClass.setmName("Jasper");
-//        assertEquals("Jasper", testClass.getmName());
-//
-//        testClass.setfName("Shey");
-//        assertEquals("Shey", testClass.getfName());
         assertEquals("Shey(D)", testCandidate.getName());
->>>>>>> 1f451168370976755fb16b43645ddaacc3b26ab7
     }
 
     @Test
     void getNumVotes()
     {
-<<<<<<< HEAD
-       /* numVotes testClass = new numVotes();
-
-        testClass.setmNumVotes("6");
-        assertEquals("6",testClass.numVotes());*/
-=======
         assertEquals(1, testCandidate.getNumVotes());
->>>>>>> 1f451168370976755fb16b43645ddaacc3b26ab7
     }
 
     @Test
     void setNumVotes()
     {
-<<<<<<< HEAD
-
-=======
-//        setNumVotes testClass = new setVotes();
-//
-//        testClass.setNumVotes("6");
-//        assertEquals("6", )
         testCandidate.setNumVotes(5);
         assertEquals(5, testCandidate.getNumVotes());
->>>>>>> 1f451168370976755fb16b43645ddaacc3b26ab7
     }
 
     @Test
