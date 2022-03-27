deeb79c68567ee8ce2eabba24ce709e31b08d69f
huynh441 <huynh441@umn.edu>
2022-03-27 18:15:34 -0500

    Create ElectionIRVTest.java

---
+++ Project1/src/main/java/Tests/Elections/ElectionIRVTest.java
@@ -0,0 +1,21 @@
+package Elections;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ElectionIRVTest
+{
+    Election IRVstandard = new Election("../IRVTest.txt");
+
+    @Test
+    void calcResults()
+    {
+
+    }
+
+    @Test
+    void generateReport()
+    {
+    }
+}
