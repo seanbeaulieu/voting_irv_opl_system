6b49b1d8fd28f7db005ad984644dc6cd2d6cbf52
spipsdew <45749527+spipsdew@users.noreply.github.com>
2022-03-27 16:03:56 -0500

    junit

---
+++ Project1 junit/src/test/java/Candidates/CandidateTest.java
@@ -0,0 +1,27 @@
+package Candidates;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CandidateTest
+{
+
+    @org.junit.jupiter.api.Test
+    void getName()
+    {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNumVotes()
+    {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setNumVotes()
+    {
+    }
+
+    @org.junit.jupiter.api.Test
+    void compareTo()
+    {
+    }
+}
