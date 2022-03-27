978f47ccbbd920999cddac534b8a45de7c5cd2e4
spipsdew <45749527+spipsdew@users.noreply.github.com>
2022-03-27 15:06:08 -0500

    this is how i'm envisioning the test files to look

---
+++ Project 1 (Jasper Edition)/src/Tests/MainTest.java
@@ -0,0 +1,19 @@
+package Tests;
+
+public class MainTest
+{
+    public MainTest()
+    {
+
+    }
+
+    public void runTests()
+    {
+        setFileName();
+        runOPL();
+        runIRV;
+        runElection();
+        generateReport();
+        displayWinners();
+    }
+}
