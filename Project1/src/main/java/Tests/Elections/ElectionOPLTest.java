aa937eb1ebf614dd3470a92f221eeec3335462a6
huynh441 <huynh441@umn.edu>
2022-03-27 18:16:01 -0500

    Create ElectionOPLTest.java

---
+++ Project1/src/main/java/Tests/Elections/ElectionOPLTest.java
@@ -0,0 +1,20 @@
+package Elections;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ElectionOPLTest
+{
+
+    @Test
+    void calcResults()
+    {
+        assertEquals()
+    }
+
+    @Test
+    void generateReport()
+    {
+    }
+}
