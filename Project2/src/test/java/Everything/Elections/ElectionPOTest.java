1b735d1b6f7c7f4dc92a8356561f29cf6094c005
beaul116 <beaul116@umn.edu>
2022-04-30 03:06:22 -0500

    electionPO test done
    csv and po can be run in same test

---
+++ Project2/src/test/java/Everything/Elections/ElectionPOTest.java
@@ -0,0 +1,35 @@
+package Everything.Elections;
+
+import Everything.Candidates.Candidate;
+import Everything.Elections.ElectionPO;
+import Everything.FileHandler;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ElectionPOTest {
+
+    @Test
+    @DisplayName("Test to Retrieve A Single Winner VIA csv file using PO")
+    void calculatePO() {
+        FileHandler fileHandler = new FileHandler();
+        String file1 = "./testing/testPO.csv";
+        fileHandler.addFilename(file1);
+        Election election;
+        election = new ElectionPO(fileHandler, false);
+        Candidate pike = new Candidate("[Pike,D]");
+
+        if (election.readInputs() == true) {
+            election.calcResults();
+        }
+
+        final ArrayList<Candidate> candidates = new ArrayList<>();
+        candidates.add(pike);
+
+        assertEquals(1, election.getWinners().size());
+    }
+}
